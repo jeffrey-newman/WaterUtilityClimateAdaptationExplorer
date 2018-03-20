362f01b8ebfa15c010a50d05af28fd0ecdab1edd
jeffrey-newman <jeffrey.neman.au@gmail.com>
2018-03-20 15:50:19 +1030

    Fixed reactive problem with updating climate pressures based on impact receivers, and started specfying queries to pull out relevant data.

--- RShinyInterface/SourceWatersExplorer/app.R
+++ RShinyInterface/SourceWatersExplorer/app.R
@@ -8,6 +8,21 @@
 #
 
 library(shiny)
+library(ODB)
+library(purrr)
+decision_database_path <- '/home/a1091793/Developer/WaterUtilityClimateAdaptationDecisionDatabaseExplorer/WaterUtilityClimateAdaptationDecisionDatabase.odb'
+core_area <- 'Natural environments'
+odb <- odb.open(decision_database_path)
+receivers_query <- paste("SELECT \"Class type\" FROM \"Vulnerabilities\" WHERE \"Core area\" = '", core_area, "'", sep="")
+odb.queries(odb,"receivers_query") = receivers_query
+receivers_list <- unique(odb.read(odb, odb.queries(odb, "receivers_query")))
+names(receivers_list) <- NULL
+
+climatePressures <- function(odb, receivers_list)
+{
+    climate_pressures_query <- paste("SELECT \"Class type\" FROM \"Vulnerabilities\" WHERE \"Core area\" = '", core_area, "'", sep="")
+    map(receivers_list, {function(receiver) return(paste("AND \"Class type\" = 'Waterway health'"))})
+}
 
 source_water_impact_receivers <- c("Surface water", "Stormwater", "Rainwater")
 impact_receivers_choice <- "Surface water"
@@ -23,13 +38,17 @@ ui <- fluidPage(
    titlePanel("Approaches, models and data explorer:\n Water utility source waters"),
    
    tabsetPanel(
+     id = "main_panel",
      tabPanel(
        "Receiver of impact",
        checkboxGroupInput(
          "receiver_select", 
          "Select the climate impact receivers you are assessing", 
-         choices=source_water_impact_receivers
+         choices=as.list(receivers_list[1]),
+         width = '100%'
          ),
+       # A text output for debugging.  Comment out following line when releasing
+       verbatimTextOutput("receiver_choice_text"),
        actionButton("receiver_next", "Next")
      ),
      tabPanel(
@@ -41,8 +60,9 @@ ui <- fluidPage(
          "Select what climate vulnerabilities/impacts you are assessing",
          choices = impacts_list
        ),
-       actionButton("impacts_next", "Next"),
-       verbatimTextOutput("impacts_choice_text")
+       # A text output for debugging.  Comment out following line when releasing
+       verbatimTextOutput("impact_choice_text"),
+       actionButton("impacts_next", "Next")
      ),
      tabPanel(
        "Climate pressures"
@@ -101,13 +121,18 @@ server <- function(input, output, session) {
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
    
-   impact_receivers_choice <- eventReactive(input$receiver_next, {
+   impact_receivers_choice <- observeEvent(input$receiver_next, {
      if(input$receiver_select[1] == "Surface water") impacts_list <- surface_water_impacts
      if(input$receiver_select[1] == "Stormwater") impacts_list <- stormwater_impacts
      if(input$receiver_select[1] == "Rainwater") impacts_list <- rainwater_impacts
      updateCheckboxGroupInput(session, "impact_select", choices = impacts_list)
-     return(impacts_list)
+     output$receiver_choice_text <- renderText(impacts_list)
+     updateTabsetPanel(session,"main_panel","Climate vulnerability/impact")
      })
+   
+   # output$receiver_choice_text <- renderText(impact_receivers_choice)
+   
+   
    
    
 }
